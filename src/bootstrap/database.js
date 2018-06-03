238520cd8f95fca90ee68b648ae487fb50cbe439
Lars Ostermann <derlarsen@gmail.com>
2018-06-03 14:05:09 +0200

    Add auth repository #1

--- src/bootstrap/database.js
+++ src/bootstrap/database.js
@@ -15,8 +15,15 @@ module.exports = async ({config, logger}) => {
   await db.createCollection('accounts');
   await db.createCollection('users');
   await db.createCollection('realms');
+  await db.createCollection('auths');
+
   await db.collection('accounts').createIndexes([realmAwareIdxSpec]);
   await db.collection('users').createIndexes([realmAwareIdxSpec]);
   await db.collection('realms').createIndexes([customIdIdxSpec]);
+  await db.collection('auths').createIndexes([
+    realmAwareIdxSpec,
+    {key: {token: 1}, name: 'token', unique: true},
+  ]);
+
   logger.debug('Database collections and indexes created');
 };
---
+++ src/repositories/auth.js
@@ -0,0 +1,37 @@
+const authModel = require('../models/auth');
+const RealmAwareRepository = require('./lib/realm-aware');
+
+class AuthRepository extends RealmAwareRepository {
+  get collection() {
+    return this.db.collection('auths');
+  }
+
+  /**
+   * @param {string} token
+   * @return {Promise<AuthModel|null>}
+   */
+  async findOneByToken(token) {
+    const auth = await this.collection.findOne({token});
+
+    return auth ? this.toModel(auth) : null;
+  }
+
+  async findByUserIdAndRemove(realmId, userId) {
+    this.collection.deleteMany({realmId, userId});
+  }
+
+  toModel(data) {
+    return authModel({
+      id: data.id,
+      userId: data.userId,
+      isOnline: data.isOnline,
+      token: data.token,
+      scope: data.scope,
+      description: data.description,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt,
+    });
+  }
+}
+
+module.exports = AuthRepository;
