1d5cfec50c6716f80d250a5b7381282794ab9ba2
Lars Ostermann <derlarsen@gmail.com>
2018-06-03 13:46:27 +0200

    Add database bootstrapping #1

---
+++ src/bootstrap/database.js
@@ -0,0 +1,22 @@
+const {MongoClient} = require('mongodb');
+
+module.exports = async ({config, logger}) => {
+  const client = await MongoClient.connect(config.database.url);
+  logger.debug('Database connected successfully');
+
+  const db = client.db('realmq');
+  const realmAwareIdxSpec = {
+    key: {realmId: 1, id: 1},
+    name: 'id_realmId',
+    unique: true,
+  };
+  const customIdIdxSpec = {key: {id: 1}, name: 'id', unique: true};
+
+  await db.createCollection('accounts');
+  await db.createCollection('users');
+  await db.createCollection('realms');
+  await db.collection('accounts').createIndexes([realmAwareIdxSpec]);
+  await db.collection('users').createIndexes([realmAwareIdxSpec]);
+  await db.collection('realms').createIndexes([customIdIdxSpec]);
+  logger.debug('Database collections and indexes created');
+};
