3838021c18e207b60cda79ce3519c97c663a532a
Alrik Zachert <az@kreatiefer.de>
2018-06-04 15:40:26 +0200

    Slice out bootstrapping logic. #1

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
-const {json: jsonBodyParser} = require('body-parser');
-const compression = require('compression');
-const express = require('express');
 const initBrokerTasks = require('./tasks/broker');
 const initBrokerMiddleware = require('./api/broker/bootstrap');
-const bootstrapDatabase = require('./bootstrap/database');
+const bootstrap = require('./bootstrap');
 
 /**
- * @param {Object} logger the logging object
- * @return {Promise<{http: Function}>} bootstrapped resources
+ * Bootstrap api.
+ *
+ * @return {Promise<Object>} bootstrapped api resource
  */
-const bootstrap = async ({logger}) => {
-  const http = express();
-  http.use(compression());
-  http.use(jsonBodyParser());
-  http.use((req, res, next) => {
-    logger.info(`HTTP: ${req.method} ${req.url}`);
-    next();
-  });
-
+const bootstrapApi = async () => {
   const brokerTasks = initBrokerTasks({
     loadAuth: _ => null,
     loadTopicPermissions: _ => ({read: false, write: false})
@@ -27,7 +17,7 @@ const bootstrap = async ({logger}) => {
   const brokerMiddleware = initBrokerMiddleware(brokerTasks);
   const api = {broker: {v1: brokerMiddleware}};
 
-  return {http, tasks, api};
+  return api;
 };
 
 module.exports = ({config, logger}) => {
@@ -42,13 +32,10 @@ module.exports = ({config, logger}) => {
       started = true;
       logger.debug('starting...');
 
-      const {http, api} = await bootstrap({config, logger});
-
+      const {http} = await bootstrap({config, logger});
+      const api = await bootstrapApi();
       http.use('/broker/v1', api.broker.v1);
 
-      await bootstrapDatabase({config, logger});
-
-      http.use((req, res) => res.status(404).send());
       httpServer = http.listen(config.http.port);
 
       logger.info('started');
---
+++ src/bootstrap/http.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const compression = require('compression');
+const {json: jsonBodyParser} = require('body-parser');
+
+/**
+ * Initialize express http app.
+ *
+ * @param {Object} logger Logger
+ * @return Promise<Object> Express App
+ */
+module.exports = async ({logger}) => {
+  const http = express();
+  http.use(compression());
+  http.use(jsonBodyParser());
+  http.use((req, res, next) => {
+    logger.info(`HTTP: ${req.url}`);
+    next();
+  });
+
+  http.use((req, res) => res.status(404).send());
+
+  return http;
+};
