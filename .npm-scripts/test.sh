74f87d9e93fcc7fbcafc8959be68d2612578514b
baranga <baranga@posteo.de>
2018-06-04 10:19:56 +0200

    Add jest testing framework

---
+++ .npm-scripts/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# npm exports original working directory as INIT_CWD
+if [ -n "$INIT_CWD" ]; then
+  cd "$INIT_CWD"
+fi
+
+if [ "$#" = "0" ]; then
+  set -ex
+  npm run lint
+  exec jest
+else
+  set -x
+  exec jest $@
+fi
