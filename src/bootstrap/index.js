d0c2528f4970918c493518da4c2af44b0a8dc630
Alrik Zachert <az@kreatiefer.de>
2018-06-04 15:40:26 +0200

    Slice out bootstrapping logic. #1

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,3 +1,18 @@
-module.exports = {
-  database: require('./database'),
+const bootstrapDatabase = require('./database');
+const bootstrapHttp = require('./http');
+
+/**
+ * Bootstrap all services.
+ *
+ * @param {Object} config Configuration
+ * @param {Object} logger Logger
+ * @return {Promise<{db: Object, http: Object}>} Resolves with bootstrapped services.
+ */
+module.exports = async ({config, logger}) => {
+  const [db, http] = await Promise.all([
+    bootstrapDatabase({config, logger}),
+    bootstrapHttp({config, logger}),
+  ]);
+
+  return {db, http};
 };
