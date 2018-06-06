5a9fb081eb3d9fa5a606d34611406485b82d5917
Alrik Zachert <az@kreatiefer.de>
2018-06-06 16:01:31 +0200

    Introduce channel model. #2

---
+++ src/models/channel.js
@@ -0,0 +1,30 @@
+const stripUndefined = require('../lib/strip-undefined');
+
+/**
+ * @typedef {Object} ChannelModel
+ * @param {string} id
+ * @param {string} realmId
+ * @param {Object} features
+ * @param {Object} [properties]
+ * @param {Date} [createdAt]
+ * @param {Date} [updatedAt]
+ */
+
+/**
+ * @return {ChannelModel} The generalized channel model
+ */
+module.exports = ({
+  id,
+  realmId,
+  features,
+  properties,
+  createdAt,
+  updatedAt
+}) => stripUndefined({
+  id,
+  realmId,
+  features,
+  properties,
+  createdAt,
+  updatedAt
+});
