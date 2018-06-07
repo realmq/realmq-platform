106a4432116aa1565401b04a925a782b9b6dda17
baranga <baranga@posteo.de>
2018-06-07 14:37:29 +0200

    Switch to request style param for list realms task (#4)

--- src/api/admin/v1/routes/realms.js
+++ src/api/admin/v1/routes/realms.js
@@ -1,7 +1,8 @@
 module.exports = tasks => ({
   get: async (req, res) => {
     const {offset = 0, limit = 20} = req.query;
-    const {ok, result: list, error} = await tasks.admin.listRealms(req.account, offset, limit);
+    const {ok, result: list, error} =
+      await tasks.admin.listRealms({account: req.account, offset, limit});
     if (!ok) {
       throw error;
     }
--- src/tasks/admin/list-realms.js
+++ src/tasks/admin/list-realms.js
@@ -1,17 +1,16 @@
 const {success} = require('../../lib/result');
 
 /**
- *
  * @param {RealmRepository} realmRepository Realm repository
  * @returns {AdminTasks#listRealms} Task
  */
 module.exports = ({realmRepository}) =>
   /**
    * @typedef {Function} AdminTasks#listRealms
-   * @param account
-   * @param offset
-   * @param limit
-   * @returns {Promise<PaginatedList>}
+   * @param {{id: string}} account
+   * @param {number} offset
+   * @param {number} limit
+   * @returns {Promise<PaginatedList>} Paginated list
    */
-  async (account, offset, limit) =>
+  async ({account, offset, limit}) =>
     success(await realmRepository.find({owningAccountId: account.id}, {offset, limit}));
