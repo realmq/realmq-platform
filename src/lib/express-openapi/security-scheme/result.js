a30539ac4fd39e2b33cb1c36fbd20ebea35198ca
baranga <baranga@posteo.de>
2018-06-08 20:30:40 +0200

    Improve wording (#4)

--- src/lib/express-openapi/security-scheme/result.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @param {function} callback Callback
- * @returns {{positive: function(): *, negative: function(*=): *}} Results
- */
-module.exports = callback => ({
-  positive: () => callback(null, true),
-  negative: status => callback(status, false),
-});
