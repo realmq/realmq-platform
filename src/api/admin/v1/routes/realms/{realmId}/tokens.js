ebf04f91691dd6fc7fb93102a9c7d3e6c9ff3fcf
Alrik Zachert <az@kreatiefer.de>
2018-06-08 14:26:58 +0200

    Add realm token endpoints. #4

---
+++ src/api/admin/v1/routes/realms/{realmId}/tokens.js
@@ -0,0 +1,39 @@
+module.exports = tasks => ({
+  get: async (req, res) => {
+    const {account, query: {offset, limit}, params: {realmId}} = req;
+    const {ok, result: list, error} =
+      await tasks.admin.listRealmTokens({account, realmId, offset, limit});
+
+    if (ok) {
+      return res.json(list);
+    }
+
+    if (error.name === 'TaskError' && error.reason === 'MissingAccessRights') {
+      return res.status(403).json({
+        code: 'ACCESS_DENIED',
+        message: error.message,
+      });
+    }
+
+    return Promise.reject(error);
+  },
+
+  post: async (req, res) => {
+    const {account, params: {realmId}, body: {id, userId, scope, description}} = req;
+    const {ok, result: realmToken, error} =
+      await tasks.admin.createRealmToken({account, realmId, id, userId, scope, description});
+
+    if (ok) {
+      return res.status(201).json(realmToken);
+    }
+
+    if (error.name === 'TaskError' && error.reason === 'MissingAccessRights') {
+      return res.status(403).json({
+        code: 'ACCESS_DENIED',
+        message: error.message,
+      });
+    }
+
+    return Promise.reject(error);
+  },
+});
