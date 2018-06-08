763e6b71f8458608c98fb580450ba1a28fd6384d
Alrik Zachert <az@kreatiefer.de>
2018-06-08 14:25:07 +0200

    Bootstrap new realm token tasks. #1

--- src/tasks/admin/index.js
+++ src/tasks/admin/index.js
@@ -1,20 +1,30 @@
 const initAuthenticateAccount = require('./authenticate-account');
 const initCreateAccount = require('./create-account');
 const initCreateRealm = require('./create-realm');
+const initCreateRealmToken = require('./create-realm-token');
 const initFetchRealm = require('./fetch-realm');
 const initListRealms = require('./list-realms');
+const initListRealmTokens = require('./list-realm-tokens');
 
 /** @typedef {object} AdminTasks */
 /**
+ * @param {CommonTasks} commonTasks Shared sub tasks
  * @param {AccountRules} accountRules The account rules
+ * @param {RealmRepository} realmRepository The realm repository
+ * @param {AuthRepository} authRepository The auth repository
  * @param {AccountRepository} accountRepository The account repository
- * @param {RealmRepository} realmRepository The real repository
  * @returns {AdminTasks} Initialized admin tasks
  */
-module.exports = ({accountRules, accountRepository, realmRepository}) => ({
-  authenticateAccount: initAuthenticateAccount({accountRules, accountRepository}),
-  createAccount: initCreateAccount({accountRules, accountRepository}),
-  createRealm: initCreateRealm({realmRepository}),
-  fetchRealm: initFetchRealm({realmRepository}),
-  listRealms: initListRealms({realmRepository}),
-});
+module.exports = ({commonTasks, accountRules, accountRepository, authRepository, realmRepository}) => {
+  const fetchRealm = initFetchRealm({realmRepository});
+
+  return {
+    fetchRealm,
+    authenticateAccount: initAuthenticateAccount({accountRules, accountRepository}),
+    createAccount: initCreateAccount({accountRules, accountRepository}),
+    createRealm: initCreateRealm({realmRepository}),
+    createRealmToken: initCreateRealmToken({fetchRealm, upsertUser: commonTasks.upsertUser, authRepository}),
+    listRealms: initListRealms({realmRepository}),
+    listRealmTokens: initListRealmTokens({fetchRealm, authRepository}),
+  };
+};
