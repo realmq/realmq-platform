cacf21f769821a444ecd351cb9224016f4d49fab
Alrik Zachert <az@kreatiefer.de>
2018-06-08 17:03:34 +0200

    Add controller for creating realms. #2

--- src/api/admin/v1/routes/realms.js
+++ src/api/admin/v1/routes/realms.js
@@ -1,5 +1,9 @@
+/**
+ * @param {{admin: AdminTasks}} tasks The task injection
+ * @return {object} The express-openapi router
+ */
 module.exports = tasks => ({
-  get: async (req, res) => {
+  async get(req, res) {
     const {offset, limit} = req.query;
     const {ok, result: list, error} =
       await tasks.admin.listRealms({account: req.account, offset, limit});
@@ -7,5 +11,16 @@ module.exports = tasks => ({
       throw error;
     }
     return res.json(list);
+  },
+
+  async post(req, res) {
+    const {account, body: {name}} = req;
+    const {ok, result: realm, error} = await tasks.admin.createRealm({account, name});
+
+    if (ok) {
+      return res.status(201).json(realm);
+    }
+
+    return Promise.reject(error);
   },
 });
