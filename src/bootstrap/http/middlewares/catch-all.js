413b004663df20c3a195c3a5e2ad275cb6816b39
Alrik Zachert <az@kreatiefer.de>
2018-06-11 10:53:14 +0200

    Improve verbosity of calls to unhandled urls. #4

--- src/bootstrap/http/middlewares/catch-all.js
+++ src/bootstrap/http/middlewares/catch-all.js
@@ -1,2 +1,5 @@
 module.exports = () =>
-  (req, res) => res.status(404).send();
+  (req, res) => res.status(404).json({
+    code: 'ENDPOINT_NOT_FOUND',
+    message: `${req.path} does not exist.`,
+  });
