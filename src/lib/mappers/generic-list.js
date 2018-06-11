6ba0806b1915ff37d5937621f08c07089103d716
Alrik Zachert <az@kreatiefer.de>
2018-06-11 17:04:38 +0200

    Add user mappers. #1

--- src/lib/mappers/generic-list.js
+++ src/lib/mappers/generic-list.js
@@ -1,3 +1,10 @@
+/**
+ * @typedef {Function} GenericListMapper
+ * @template DataModel, ViewModel
+ * @param {PaginatedList<DataModel>} list The list
+ * @param {function(DataModel):ViewModel} mapItem Item mapping function
+ * @return {PaginatedList<ViewModel>} The mapped result list
+ */
 module.exports = ({
   list: {limit = 0, offset = 0, count = null, total = null, items = []},
   mapItem = (i => i),
