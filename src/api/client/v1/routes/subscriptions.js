1f40be47af54269657bc548d9819719d064b244c
baranga <baranga@posteo.de>
2018-06-11 14:11:12 +0200

    Add client api controllers for subscriptions (#2)

---
+++ src/api/client/v1/routes/subscriptions.js
@@ -0,0 +1,41 @@
+/**
+ * @param {object} tasks Tasks
+ * @param {ClientTasks} tasks.client Tasks
+ * @param {object} mappers Mappers
+ * @returns {{get: get, post: post}} Method handlers
+ */
+module.exports = (tasks, mappers) => ({
+  /**
+   * GET /subscriptions/{id}
+   * @param {object} req Request
+   * @param {object} res Response
+   */
+  get: async (req, res) => {
+    const {auth: authToken, query: {offset, limit}} = req;
+    const {ok, value: list, error} =
+      await tasks.client.listSubscriptions({authToken, offset, limit});
+
+    if (!ok) {
+      throw error;
+    }
+
+    res.json(mappers.subscriptionList(list));
+  },
+  /**
+   * POST /subscriptions/{id}
+   * @param {object} req Request
+   * @param {object} res Response
+   */
+  post: async (req, res) => {
+    const {auth: authToken, body: data} = req;
+
+    const {ok, value: subscription, error} =
+      await tasks.client.createSubscription({authToken, data});
+
+    if (!ok) {
+      throw error;
+    }
+
+    res.status(201).json(mappers.subscription(subscription));
+  },
+});
--- src/api/client/v1/routes/subscriptions/{id}.js
+++ src/api/client/v1/routes/subscriptions/{id}.js
@@ -1,58 +1,53 @@
 /**
- *
  * @param {object} tasks Tasks
- * @param {ClientTasks} tasks.client Client tasks
+ * @param {ClientTasks} tasks.client Tasks
  * @param {object} mappers Mappers
  * @returns {{get: get, patch: patch, delete: delete}} Method handlers
  */
 module.exports = (tasks, mappers) => ({
   /**
-   * GET /channel/{id}
+   * GET /subscriptions/{id}
    * @param {object} req Request
    * @param {object} res Response
    */
   get: async (req, res) => {
     const {auth: authToken, params: {id}} = req;
 
-    const {ok, result: channel, error} = await tasks.client.fetchChannel({authToken, id});
+    const {ok, result: subscription, error} = await tasks.client.fetchSubscription({authToken, id});
     if (!ok) {
       throw error;
     }
 
-    if (!channel) {
-      return res.status(404).json({
-        code: 'UnknownChannel',
-        message: 'Channel does not exists.',
-      });
+    if (subscription === null) {
+      res.status(404);
+    } else {
+      res.json(mappers.subscription(subscription));
     }
-
-    res.json(mappers.channel(channel));
   },
-
   /**
-   * PATCH /channel/{id}
+   * PATCH /subscriptions/{id}
    * @param {object} req Request
    * @param {object} res Response
    */
   patch: async (req, res) => {
     const {auth: authToken, params: {id}, body: patch} = req;
 
-    const {ok, result: channel, error} = await tasks.client.patchChannel({authToken, id, patch});
+    const {ok, result: subscription, error} = await tasks.client.patchSubscription({authToken, id, patch});
     if (!ok) {
       throw error;
     }
 
-    res.json(mappers.channel(channel));
+    res.json(mappers.subscription(subscription));
   },
   /**
-   * DELETE /channel/{id}
+   * DELETE /subscriptions/{id}
    * @param {object} req Request
    * @param {object} res Response
    */
   delete: async (req, res) => {
     const {auth: authToken, params: {id}} = req;
 
-    const {ok, error} = await tasks.client.deleteChannel({authToken, id});
+    const {ok, error} = await tasks.client.deleteSubscription({authToken, id});
     if (!ok) {
       throw error;
     }
