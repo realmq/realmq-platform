e704d7069caaef0a2f5be9cfd8af18a13c06377d
Alrik Zachert <az@kreatiefer.de>
2018-06-11 17:32:15 +0200

    Pass missing logger dependency to client middleware initialization.

--- src/bootstrap/http/middlewares/index.js
+++ src/bootstrap/http/middlewares/index.js
@@ -5,14 +5,14 @@ const initCatchAll = require('./catch-all');
 const initError = require('./error');
 
 /**
- *
  * @param {object} tasks Tasks
+ * @param {Logger} logger The logger
  * @returns {{admin: function, broker: function, catchAll: function, error: function}} Middlewares
  */
 module.exports = async ({tasks, logger}) => ({
   admin: await initAdmin({tasks, logger}),
   broker: await initBroker({tasks}),
-  client: await initClient({tasks}),
+  client: await initClient({tasks, logger}),
   catchAll: await initCatchAll({logger}),
   error: await initError({logger}),
 });
