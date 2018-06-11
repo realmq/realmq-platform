76aacb35797a9da6f893c16a37558daf48e059c0
baranga <baranga@posteo.de>
2018-06-11 09:58:35 +0200

    Init create and list channels tasks (#2)

--- src/tasks/client/index.js
+++ src/tasks/client/index.js
@@ -1,11 +1,23 @@
 const initAuthenticateUser = require('./authenticate-user');
+const initCreateChannel = require('./create-channel');
+const initListChannels = require('./list-channels');
 
 /** @typedef {object} ClientTasks */
 /**
  * Initialize client tasks
  * @param {AuthRepository} authRepository Auth repository
+ * @param {ChannelRepository} channelRepository Auth repository
+ * @param {SubscriptionRepository} subscriptionRepository Auth repository
+ * @param {UserRepository} userRepository Auth repository
  * @return {ClientTasks} Initialized tasks
  */
-module.exports = ({authRepository, userRepository}) => ({
+module.exports = ({
+  authRepository,
+  channelRepository,
+  subscriptionRepository,
+  userRepository,
+}) => ({
   authenticateUser: initAuthenticateUser({authRepository, userRepository}),
+  createChannel: initCreateChannel({channelRepository}),
+  listChannels: initListChannels({channelRepository, subscriptionRepository}),
 });
