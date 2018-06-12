4e233aaea70dd540198470a38fc38cb1c8bc294f
baranga <baranga@posteo.de>
2018-06-12 20:04:34 +0200

    Extend error mapping (#2)

--- src/lib/error/map-task-to-http-error.js
+++ src/lib/error/map-task-to-http-error.js
@@ -2,13 +2,16 @@ const createHttpError = require('./http');
 
 const ERROR_CODE_TO_STATUS_MAP = {
   // 400
+  AlreadySubscribed: 400,
   AuthTokenAlreadyExists: 400,
   ChannelAlreadyExists: 400,
   UserAlreadyExists: 400,
   EmailAlreadyTaken: 400,
-  InvalidPatch: 400,
   InvalidChannel: 400,
+  InvalidPatch: 400,
+  InvalidSubscription: 400,
   InvalidUser: 400,
+  SubscriptionAlreadyExists: 400,
 
   // 403
   InsufficientPrivileges: 403,
@@ -16,6 +19,7 @@ const ERROR_CODE_TO_STATUS_MAP = {
   // 404
   UnknownChannel: 404,
   UnknownRealm: 404,
+  UnknownSubscription: 404,
   UnknownUser: 404,
 };
 
