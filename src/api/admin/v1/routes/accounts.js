c51e69abbf90b8ea3f773f58f1a457ba46f2fdf9
Alrik Zachert <az@kreatiefer.de>
2018-06-12 13:16:42 +0200

    Map account responses.
    resolves #31

--- src/api/admin/v1/routes/accounts.js
+++ src/api/admin/v1/routes/accounts.js
@@ -1,9 +1,10 @@
 /**
  * @param {object} tasks Tasks
  * @param {AdminTasks} tasks.admin Admin tasks
+ * @param {AdminApiV1Mappers} mappers Mappers
  * @returns {object} Path handler
  */
-module.exports = tasks => ({
+module.exports = (tasks, mappers) => ({
   post: async (req, res) => {
     const {ok, result: account, error} = await tasks.admin.createAccount(req.body);
 
@@ -11,6 +12,6 @@ module.exports = tasks => ({
       throw error;
     }
 
-    res.status(201).json(account);
+    res.status(201).json(mappers.account(account));
   },
 });
