2ec993ff74d97968cfddca005d5feee3331666bf
baranga <baranga@posteo.de>
2018-06-12 08:29:25 +0200

    Bootstrap subscription crud tasks (#2)

--- src/tasks/client/index.js
+++ src/tasks/client/index.js
@@ -1,13 +1,18 @@
 const initAuthenticateUser = require('./authenticate-user');
 const initCreateChannel = require('./create-channel');
+const initCreateSubscription = require('./create-subscription');
 const initCreateUser = require('./create-user');
 const initDeleteChannel = require('./delete-channel');
+const initDeleteSubscription = require('./delete-subscription');
 const initDeleteUser = require('./delete-user');
 const initFetchChannel = require('./fetch-channel');
+const initFetchSubscription = require('./fetch-subscription');
 const initFetchUser = require('./fetch-user');
 const initListChannels = require('./list-channels');
+const initListSubscriptions = require('./list-subscriptions');
 const initListUsers = require('./list-users');
 const initPatchChannel = require('./patch-channel');
+const initPatchSubscription = require('./patch-subscription');
 const initPatchUser = require('./patch-user');
 
 /** @typedef {object} ClientTasks */
@@ -25,15 +30,36 @@ module.exports = ({
   subscriptionRepository,
   userRepository,
 }) => ({
-  authenticateUser: initAuthenticateUser({authRepository, userRepository}),
-  createChannel: initCreateChannel({channelRepository}),
-  createUser: initCreateUser({userRepository}),
-  deleteChannel: initDeleteChannel({channelRepository}),
-  deleteUser: initDeleteUser({userRepository, authRepository, subscriptionRepository}),
-  fetchChannel: initFetchChannel({channelRepository, subscriptionRepository}),
-  fetchUser: initFetchUser({userRepository}),
-  listChannels: initListChannels({channelRepository, subscriptionRepository}),
-  listUsers: initListUsers({userRepository}),
-  patchChannel: initPatchChannel({channelRepository}),
-  patchUser: initPatchUser({userRepository}),
+  authenticateUser:
+    initAuthenticateUser({authRepository, userRepository}),
+  createChannel:
+    initCreateChannel({channelRepository}),
+  createSubscription:
+    initCreateSubscription({userRepository, channelRepository, subscriptionRepository}),
+  createUser:
+    initCreateUser({userRepository}),
+  deleteChannel:
+    initDeleteChannel({channelRepository}),
+  deleteSubscription:
+    initDeleteSubscription({subscriptionRepository}),
+  deleteUser:
+    initDeleteUser({userRepository, authRepository, subscriptionRepository}),
+  fetchChannel:
+    initFetchChannel({channelRepository, subscriptionRepository}),
+  fetchSubscription:
+    initFetchSubscription({subscriptionRepository}),
+  fetchUser:
+    initFetchUser({userRepository}),
+  listChannels:
+    initListChannels({channelRepository, subscriptionRepository}),
+  listSubscriptions:
+    initListSubscriptions({subscriptionRepository}),
+  listUsers:
+    initListUsers({userRepository}),
+  patchChannel:
+    initPatchChannel({channelRepository}),
+  patchSubscription:
+    initPatchSubscription({subscriptionRepository}),
+  patchUser:
+    initPatchUser({userRepository}),
 });
