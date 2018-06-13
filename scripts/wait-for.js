29543bb576494405646d75b3c6fe77c67542a4fb
baranga <baranga@posteo.de>
2018-06-13 13:31:09 +0200

    Wait for smoke test services to start (#7)

---
+++ scripts/wait-for.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+/* eslint no-await-in-loop: "off" */
+
+const {createConnection} = require('net');
+
+const connect = (host, port) => new Promise((resolve, reject) => {
+  const connection = createConnection(host, port);
+  connection.once('connect', () => resolve(connection));
+  connection.once('error', err => reject(err));
+});
+
+const test = async (host, port) => {
+  try {
+    await connect(host, port);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+const main = async argv => {
+  const [host, port, timeout = 30] = argv.slice(2);
+  if (!host || !port) {
+    console.error(`usage: ${argv[1]} <host> <port> [timeout]`);
+    return 2;
+  }
+
+  const start = Date.now();
+  let present = false;
+  while (!present) {
+    if (Date.now() - start > (timeout * 1000)) {
+      throw new Error(`Timeout connecting to ${host}:${port}`);
+    }
+    await sleep(980);
+    present = await test(host, port);
+  }
+};
+
+(async () => {
+  try {
+    const code = await main(process.argv);
+    process.exit(code);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+})();
