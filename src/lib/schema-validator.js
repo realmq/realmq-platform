1b1903eee8ea6cc6ae836896f3ccc4957c65e7e8
Alrik Zachert <az@kreatiefer.de>
2018-06-13 13:30:24 +0200

    Improve schema validator based on dev review.

--- src/lib/schema-validator.js
+++ src/lib/schema-validator.js
@@ -1,6 +1,15 @@
 const Ajv = require('ajv');
 
 /**
- * @type {ajv | ajv.Ajv}
+ * Build validator
+ * @param {object} schema Schema to build validator for
+ * @returns {function(object): {valid: boolean, errors: object[]}} Validator
  */
-module.exports = new Ajv();
+module.exports = schema => {
+  const ajv = new Ajv();
+  const validate = ajv.compile(schema);
+  return subject => {
+    const valid = validate(subject);
+    return {valid, errors: validate.errors || []};
+  };
+};
