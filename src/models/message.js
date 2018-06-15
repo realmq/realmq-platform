1684f752ce318cd93552b37e038f41312d6042da
Alrik Zachert <az@kreatiefer.de>
2018-06-15 13:48:26 +0200

    Introduce message model. #3

---
+++ src/models/message.js
@@ -0,0 +1,30 @@
+const stripUndefined = require('../lib/strip-undefined');
+
+/**
+ * @typedef {Object} MessageModel
+ * @param {string} id
+ * @param {string} realmId
+ * @param {string} channelId
+ * @param {string} content
+ * @param {Date} createdAt
+ * @param {Date} updatedAt
+ */
+
+/**
+ * @return {MessageModel} The generalized persisted message model
+ */
+module.exports = ({
+  id,
+  realmId,
+  channelId,
+  content,
+  createdAt,
+  updatedAt,
+}) => stripUndefined({
+  id,
+  realmId,
+  channelId,
+  content,
+  createdAt,
+  updatedAt,
+});
