d1c2a07f6bda15cc6882442078e955b60e8818d6
Alrik Zachert <az@kreatiefer.de>
2018-06-15 13:47:32 +0200

    Introduce message repository. #3

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,6 +1,7 @@
 const createAccountRepository = require('./account');
 const createAuthRepository = require('./auth');
 const createChannelRepository = require('./channel');
+const createMessageRepository = require('./message');
 const createRealmRepository = require('./realm');
 const createSubscriptionRepository = require('./subscription');
 const createUserRepository = require('./user');
@@ -11,20 +12,22 @@ const createUserRepository = require('./user');
  * @param {Collection} accountCollection Dependency
  * @param {Collection} authCollection Dependency
  * @param {Collection} channelCollection Dependency
+ * @param {Collection} messageCollection Dependency
  * @param {Collection} realmCollection Dependency
  * @param {Collection} subscriptionCollection Dependency
  * @param {Collection} userCollection Dependency
  * @return {Repositories} The repositories
  */
 module.exports = ({
-  accountCollection, authCollection, channelCollection, realmCollection,
-  subscriptionCollection, userCollection,
+  accountCollection, authCollection, channelCollection, messageCollection,
+  realmCollection, subscriptionCollection, userCollection,
 }) =>
   /**
    * @typedef {object} Repositories
    * @property {AccountRepository} account
    * @property {AuthRepository} auth
    * @property {ChannelRepository} channel
+   * @property {MessageRepository} message
    * @property {RealmRepository} realm
    * @property {SubscriptionRepository} subscription
    * @property {UserRepository} user
@@ -33,6 +36,7 @@ module.exports = ({
     account: createAccountRepository({collection: accountCollection}),
     auth: createAuthRepository({collection: authCollection}),
     channel: createChannelRepository({collection: channelCollection}),
+    message: createMessageRepository({collection: messageCollection}),
     realm: createRealmRepository({collection: realmCollection}),
     subscription: createSubscriptionRepository({collection: subscriptionCollection}),
     user: createUserRepository({collection: userCollection}),
