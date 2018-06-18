2bfc4650fee7b65f2d4b9d34c7ed1d33630be3b3
baranga <baranga@posteo.de>
2018-06-18 13:33:21 +0200

    Extend http errors by common error cases (#6)

--- src/lib/error/http.js
+++ src/lib/error/http.js
@@ -4,17 +4,44 @@
  * @param {string} code The error code
  * @return {HttpError} The http error object
  */
-module.exports = ({status, message, code}) =>
+const error = ({status, message, code, previous}) =>
   /**
    * @class HttpError
    * @property {number} status The http status code
    * @property {string} message The error message
    * @property {string} code The error code
    * @property {boolean} isHttpError Flag that indicates http error type.
+   * @property {object} [previous] Previous error
    */
   ({
     isHttpError: true,
     status,
     message,
     code,
+    previous,
   });
+
+/**
+ * Not found error
+ * @param {string} path
+ * @returns {HttpError}
+ */
+error.notFound = ({path}) => error({
+  status: 404,
+  code: 'EndpointNotFound',
+  message: `${path} does not exist.`,
+});
+
+/**
+ * Internal server error
+ * @param {object} [previous] Previous error
+ * @returns {HttpError}
+ */
+error.internal = ({previous} = {}) => error({
+  status: 500,
+  code: 'InternalServerError',
+  message: 'The request could not be processed.',
+  previous,
+});
+
+module.exports = error;
