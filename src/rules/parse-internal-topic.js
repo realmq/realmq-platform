dab5e5e3984ece6ca6f3c24a12c7fd7040ce71f3
Alrik Zachert <az@kreatiefer.de>
2018-06-26 10:56:58 +0200

    Add task for recording messages (aka. persistence). #3

---
+++ src/rules/parse-internal-topic.js
@@ -0,0 +1,57 @@
+const isSysTopicLevel = topicLevel => topicLevel[0] === '$';
+const isRmqSysTopicLevel = topicLevel => topicLevel === this.rmqSysTopic;
+
+/**
+ * Parse internal topic.
+ *
+ * @param {string} topic Topic
+ * @return {TopicParseResult} Topic parse result
+ */
+module.exports = topic => {
+  const parts = topic.split('/');
+  const isSysTopic = isSysTopicLevel(parts[0]);
+  const isRmqSysTopic = isRmqSysTopicLevel(parts[0]);
+
+  if (!isRmqSysTopic && isSysTopic) {
+    // Don't handle it
+    return null;
+  }
+
+  // Remove "$RMQ"
+  if (isRmqSysTopic) {
+    parts.shift();
+  }
+  // Remove "realm"
+  parts.shift();
+  // Remove ":realmId"
+  const realmId = parts.shift();
+
+  let isSync = false;
+  let userId = null;
+  if (
+    isRmqSysTopic &&
+    parts.length >= 3 &&
+    parts[0] === 'sync' &&
+    parts[1] === 'user'
+  ) {
+    // Like: sync/user/:uid
+    parts.shift();
+    isSync = true;
+    parts.shift();
+    userId = parts.shift();
+  }
+
+  /**
+   * @class TopicParseResult
+   */
+  return {
+    isRmqSysTopic,
+    isSync,
+    realmId,
+    userId,
+    topic: parts.join('/'),
+  };
+};
+
+module.exports.isSysTopicLevel = isSysTopicLevel;
+module.exports.isRmqSysTopicLevel = isRmqSysTopicLevel;
