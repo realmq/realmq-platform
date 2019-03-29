0e4003aa45b0a5cbbbe92b2d3727677e313e53ea
Alrik Zachert <az@kreatiefer.de>
2019-03-29 23:43:08 +0100

    Add `npm run dev` script for easy getting up and running. #78

---
+++ .npm-scripts/start-dev.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Stop services when user hits Ctrl+C
+on_exit() {
+  docker-compose stop
+  exit 0
+}
+
+trap "on_exit" SIGINT
+
+if [[ ! -f .env ]]; then
+  echo -e "\e[1;34m☞\e[0m Setting up \e[1m.env\e[0m file. Here you can adjust dev setup configuration."
+  echo -e "\e[1;34m☞\e[0m See \e[4msrc/config/README.md\e[0m for a complete list of configuration options."
+  echo ""
+  cp .env.template .env
+fi
+
+# make sure the latest dependencies are installed
+npm install
+
+# launch docker setup
+docker-compose up -d
+
+# what a little until mongo has started
+sleep 2
+
+# attach to the logs
+docker-compose logs -ft --tail 5 platform
