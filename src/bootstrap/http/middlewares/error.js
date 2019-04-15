a1dfbc7d1eeba4b20b4bf1d8ff1fbd4d0382f5b5
Alrik Zachert <az@kreatiefer.de>
2019-04-15 12:45:27 +0200

    Improve http error response.
    * Add support for authorization challenges
    * Include optional error details within response

--- src/bootstrap/http/middlewares/error.js
+++ src/bootstrap/http/middlewares/error.js
@@ -1,14 +1,27 @@
 const {internal: internalError} = require('../../../lib/error/http');
 
 module.exports = ({logger}) =>
-  (err, req, res, _) => {
-    if (!err.isHttpError) {
-      logger.error(`Unhandled error in http call: ${err}`, {err});
-      err = internalError({previous: err});
+  (error, req, res, _) => {
+    // Map all unknown errors to internal server errors
+    if (!error.isHttpError) {
+      logger.error(`Unhandled error in http call: ${error}`, {error});
+      error = internalError({previous: error});
     }
 
-    res.status(err.status).json({
-      code: err.code,
-      message: err.message,
-    });
+    // Set challenge headers
+    if (error.status === 401 && error.challenge) {
+      res.set('www-authenticate', error.challenge);
+    }
+
+    const response = {
+      code: error.code,
+      message: error.message,
+    };
+
+    // Include optional details
+    if (error.details) {
+      response.details = error.details;
+    }
+
+    res.status(error.status).json(response);
   };
