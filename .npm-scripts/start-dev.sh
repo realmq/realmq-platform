055dac0be762a74a654300b0be004b205058b697
Alrik Zachert <az@kreatiefer.de>
2019-06-25 12:31:56 +0200

    Use octal escape sequence, since old bash version on osx does not support "\e".
    see #93

--- .npm-scripts/start-dev.sh
+++ .npm-scripts/start-dev.sh
@@ -9,8 +9,8 @@ on_exit() {
 trap "on_exit" SIGINT
 
 if [[ ! -f .env ]]; then
-  echo -e "\e[1;34m☞\e[0m Setting up \e[1m.env\e[0m file. Here you can adjust dev setup configuration."
-  echo -e "\e[1;34m☞\e[0m See \e[4msrc/config/README.md\e[0m for a complete list of configuration options."
+  echo -e "\033[1;34m☞\033[0m Setting up \033[1m.env\033[0m file. Here you can adjust dev setup configuration."
+  echo -e "\033[1;34m☞\033[0m See \033[4msrc/config/README.md\033[0m for a complete list of configuration options."
   echo ""
   cp .env.template .env
 fi
@@ -26,20 +26,20 @@ sleep 2
 
 # copy dev root ca certificate to user space
 if [[ ! -f certs/dev-ca-root.crt.pem ]]; then
-  echo -e "\e[1;34m☞\e[0m Setting up \e[1mTLS certificates\e[0m.\nCopy dev ROOT CA certificate to \e[4mcerts/dev-ca-root.crt.pem\e[0m."
+  echo -e "\033[1;34m☞\033[0m Setting up \033[1mTLS certificates\033[0m.\nCopy dev ROOT CA certificate to \033[4mcerts/dev-ca-root.crt.pem\033[0m."
   mkdir certs
   docker cp $(docker-compose ps -q certificates | head -1):/data/certificates/root.crt.pem certs/dev-ca-root.crt.pem
 fi
 
 # install dev hosts
 if [[ ! -n "$(grep -P "[[:space:]]realmq.local" /etc/hosts)" ]]; then
-    echo -e "\e[1;34m☞\e[0m Setting up \e[1mlocal DNS\e[0m.\nAdding \e[1m127.0.0.1 realmq.local api.realmq.local rtm.realmq.local\e[0m to \e[4m/etc/hosts\e[0m."
+    echo -e "\033[1;34m☞\033[0m Setting up \033[1mlocal DNS\033[0m.\nAdding \033[1m127.0.0.1 realmq.local api.realmq.local rtm.realmq.local\033[0m to \033[4m/etc/hosts\033[0m."
     printf "127.0.0.1 realmq.local api.realmq.local rtm.realmq.local\n" | sudo tee -a "/etc/hosts" > /dev/null;
 
     if [[ -n "$(grep "realmq.local" /etc/hosts)" ]]; then
-        echo -e "\e[0;32m✓\e[0m Hosts have been added successfully"
+        echo -e "\033[0;32m✓\033[0m Hosts have been added successfully"
     else
-        echo -e "\e[1;33mFailed to setup hosts\e[0m";
+        echo -e "\033[1;33mFailed to setup hosts\033[0m";
     fi
 fi
 
