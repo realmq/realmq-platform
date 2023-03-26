792250968a40ff3f48e0159e6ade01ada047dc58
Alrik Zachert <az@kreatiefer.de>
2023-03-26 21:20:02 +0200

    Add support for deleting tokens via admin api.

---
+++ src/api/admin/v1/routes/realms/{realmId}/tokens/{id}.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param {{admin: AdminTasks}} tasks Tasks
+ * @returns {{delete: delete}} Method handlers
+ */
+module.exports = (tasks) => ({
+  /**
+   * DELETE /realms/{realmId}/auth/tokens/{id}
+   * @param {object} request Request
+   * @param {object} response Response
+   */
+  async delete(request, response) {
+    const {realmId, id} = request.params;
+
+    const {ok, error} = await tasks.admin.deleteToken({realmId, id});
+
+    if (!ok) {
+      throw error;
+    }
+
+    response.status(204).send();
+  },
+});
---
+++ src/tasks/admin/delete-token.spec.js
@@ -0,0 +1,53 @@
+const authRepository = require('../../lib/test/mocks/repositories/auth');
+const realtimeConnectionRepository = require('../../lib/test/mocks/repositories/realtime-connection');
+const realmRepository = require('../../lib/test/mocks/repositories/realm');
+const initDeleteToken = require('./delete-token');
+
+describe('The admin deleteToken task', () => {
+  let deleteToken;
+
+  beforeEach(() => {
+    authRepository.deleteOne = jest.fn();
+    realtimeConnectionRepository.deleteAllByAuthId = jest.fn();
+
+    deleteToken = initDeleteToken({
+      authRepository,
+      realmRepository,
+      realtimeConnectionRepository
+    });
+  });
+
+  describe('when given an unknown realmId id', () => {
+    it('should fail with appropriate error', async () => {
+      const {ok, error} = await deleteToken({realmId: realmRepository.unknownRealmId, id: authRepository.knownAuthId});
+
+      expect(ok).toBe(false);
+      expect(error).toBeDefined();
+      expect(error.code).toBe('UnknownRealm');
+    });
+  });
+
+  describe('when given an unknown token id', () => {
+    it('should fail with appropriate error', async () => {
+      const {ok, error} = await deleteToken({realmId: realmRepository.knownRealmId, id: authRepository.unknownAuthId});
+
+      expect(ok).toBe(false);
+      expect(error).toBeDefined();
+      expect(error.code).toBe('UnknownAuthToken');
+    });
+  });
+
+  describe('when configured correctly', () => {
+    it('should delete the auth token', async () => {
+      const {ok, result} = await deleteToken({realmId: realmRepository.knownRealmId, id: authRepository.knownAuthId});
+
+      expect(ok).toBe(true);
+      expect(authRepository.deleteOne).toHaveBeenCalled();
+      expect(realtimeConnectionRepository.deleteAllByAuthId).toHaveBeenCalledWith({
+        realmId: realmRepository.knownRealmId,
+        authId: authRepository.knownAuthId,
+      });
+      expect(result).toMatchObject(authRepository.validAuth);
+    });
+  });
+});
